14510f46ee926c6a3521a6191649a41e3e67b740
Wolkenfarmer <jomaxw2@aol.com>
2020-12-28 11:39:27 +0100

    Tabstops instead of spaces

--- Assets/Scripts/Bullets/LaserBulletBehaviorScript.cs
+++ Assets/Scripts/Bullets/LaserBulletBehaviorScript.cs
@@ -1,4 +1,4 @@
 public class LaserBulletBehaviorScript : BulletBehaviorScript
 {
-
+	// temp
 }
--- Assets/Scripts/GameBehaviorScript.cs
+++ Assets/Scripts/GameBehaviorScript.cs
@@ -3,44 +3,44 @@ using UnityEngine;
 
 public class GameBehaviorScript : MonoBehaviour
 {
-    public GameObject EnemyPrefab;
+	public GameObject EnemyPrefab;
 
-    readonly List<GameObject> enemies = new List<GameObject>();
-    int currentWave;
+	readonly List<GameObject> enemies = new List<GameObject>();
+	int currentWave;
 
-    void Start()
-    {
-        NewWave();
-    }
-
-    void Update()
-    {
-        var allAlive = false;
-        foreach (var enemy in enemies)
-        {
-            if (enemy != null)
-            {
-                allAlive = true;
-                break;
-            }
-        }
-
-        if (!allAlive)
-            NewWave();
-    }
-
-    void NewWave()
+	void Start()
 	{
-        currentWave++;
+		NewWave();
+	}
 
-        enemies.Clear();
-
-        var half = (currentWave / 2) * -1;
-        for (int i = 0; i < currentWave; i++)
+	void Update()
+	{
+		var allAlive = false;
+		foreach (var enemy in enemies)
 		{
-            var obj = Instantiate(EnemyPrefab);
-            obj.transform.position = new Vector2(half + i * 2, 7f);
-            enemies.Add(obj);
+			if (enemy != null)
+			{
+				allAlive = true;
+				break;
+			}
+		}
+
+		if (!allAlive)
+			NewWave();
+	}
+
+	void NewWave()
+	{
+		currentWave++;
+
+		enemies.Clear();
+
+		var half = (currentWave / 2) * -1;
+		for (int i = 0; i < currentWave; i++)
+		{
+			var obj = Instantiate(EnemyPrefab);
+			obj.transform.position = new Vector2(half + i * 2, 7f);
+			enemies.Add(obj);
 		}
 	}
 }
