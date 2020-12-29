88c3de4d0f53614e30effacd0347cb6a196b41b4
abc013 <abc013@alpenjodel.de>
2020-12-29 22:43:48 +0100

    Restrict player movement and allow proper enemy movement
    Add blockade that shows how high the player can fly

---
+++ Assets/Scripts/BlockadeBehaviorScript.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class BlockadeBehaviorScript : MonoBehaviour
+{
+	public GameObject Player;
+	SpriteRenderer blockadeRenderer;
+
+	Vector2 linePoint;
+	Vector2 lineDirection;
+
+	void Start()
+	{
+		blockadeRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+		linePoint = gameObject.transform.position;
+		lineDirection = Vector2.right;
+	}
+
+	void Update()
+	{
+		var playerPos = Player.transform.position;
+		var distance = (NearestPointOnLine(playerPos) - playerPos).magnitude - 1.14f;
+
+		var color = blockadeRenderer.color;
+		if (distance < 5)
+			color.a = 1 - distance / 5;
+		else
+			color.a = 0;
+
+		blockadeRenderer.color = color;
+	}
+
+	// https://forum.unity.com/threads/how-do-i-find-the-closest-point-on-a-line.340058/
+	Vector3 NearestPointOnLine(Vector2 point)
+	{
+		var v = point - linePoint;
+		var d = Vector2.Dot(v, lineDirection);
+
+		return linePoint + lineDirection * d;
+	}
+}
--- Assets/Scripts/ScreenClampBehaviorScript.cs
+++ Assets/Scripts/ScreenClampBehaviorScript.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class ScreenClampBehaviorScript : MonoBehaviour
 {
+	public bool IsPlayer;
+
 	Vector2 screenBounds;
 	Vector2 spriteBounds;
 
@@ -18,8 +20,17 @@ public class ScreenClampBehaviorScript : MonoBehaviour
 		var position = transform.position;
 
 		position.x = Mathf.Clamp(position.x, -screenBounds.x + spriteBounds.x / 2, screenBounds.x - spriteBounds.x / 2);
-		position.y = Mathf.Clamp(position.y, -screenBounds.y + spriteBounds.y / 2, screenBounds.y - spriteBounds.y / 2);
+		if (IsPlayer)
+			position.y = Mathf.Clamp(position.y, -screenBounds.y + spriteBounds.y / 2, -spriteBounds.y / 2);
+		else if (position.y < -screenBounds.y - spriteBounds.y / 2)
+			leftScreen();
 
 		transform.position = position;
+	}
+
+	void leftScreen()
+	{
+		Debug.Log($"{gameObject} left the game area.");
+		Destroy(gameObject);
 	}
 }
